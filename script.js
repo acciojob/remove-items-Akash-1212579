06e3fa848d89092d8e1400930c0ee274216ca4ad
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-25 10:47:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+let list = document.getElementById("colorSelect");
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",()=>{
+    let Value = list.value;
+for (let i = 0; i < list.length; i++) {
+   // console.log(list.length)
+    console.log(Value);
+	if (list.options[i].value == Value) {
+		list.remove(i);
+		break;
+	}
+}
+})
